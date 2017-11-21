1a05ff6e43548d7d42787d7e372b5d86f2aa6952
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2017-11-21 02:42:29 +0000

    task 1: fixed pep8 errors

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 '''
-script that generates a .tgz archive from the contents of the web_static 
+script that generates a .tgz archive from the contents of the web_static
 folder of your AirBnB Clone repo, using the function do_pack, otherwise
 return none.
 '''
 from fabric.api import *
 import time
 from datetime import date
+
 
 def do_pack():
     ymdhms = time.strftime('%Y%m%d%H%M%S')
